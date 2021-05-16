cdd495386c4da64c4ca8ce8f21ae99dfa5df5bfd
hijiki51 <hibiki0719euph@gmail.com>
2021-05-16 16:15:01 +0900

    curl

--- pprof.sh
+++ pprof.sh
@@ -5,4 +5,7 @@ go tool pprof -png -output pprof_heap.png http://localhost:6060/debug/pprof/heap
 go tool pprof -png -output pprof_block.png http://localhost:6060/debug/pprof/block?seconds=$1 &
 go tool pprof -png -output pprof_mutex.png http://localhost:6060/debug/pprof/mutex?seconds=$1 &
 wait 
-curl -X POST -F img=@pprof_profile.png img=@pprof_heap.png img=@pprof_block.png img=@pprof_mutex.png $2 
+curl -X POST -F img=@pprof_profile.png $2 &&
+curl -X POST -F img=@pprof_heap.png $2 &&
+curl -X POST -F img=@pprof_block.png $2 &&
+curl -X POST -F img=@pprof_mutex.png $2 &&
